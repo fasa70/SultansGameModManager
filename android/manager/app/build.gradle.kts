bf5fa9b8d06e57a896ae2ce702e2fffc94ae4b94
fasa70 <213198939+fasa70@users.noreply.github.com>
2026-07-31 20:28:44 +0800

    build(app): 升级版本至 0.2.0

--- android/manager/app/build.gradle.kts
+++ android/manager/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.sultansgame.modmanager"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
