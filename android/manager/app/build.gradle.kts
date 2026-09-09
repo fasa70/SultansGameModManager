8649593d2f1fb3fe1e13cb5d307b8bb591fe7112
fasa70 <213198939+fasa70@users.noreply.github.com>
2026-09-09 09:33:35 +0800

    build(app): bump version to 0.3.42

--- android/manager/app/build.gradle.kts
+++ android/manager/app/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         applicationId = "com.sultansgame.modmanager"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 341
-        versionName = "0.3.41"
+        versionCode = 342
+        versionName = "0.3.42"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += "arm64-v8a"
